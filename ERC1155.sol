c3916025e104cf318b16eee8bf964d48520ede41
Shivam Singh <shivamsingh175503@gmail.com>
2023-08-10 21:30:50 +0530

    change

--- ERC1155.sol
+++ ERC1155.sol
@@ -5,9 +5,7 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC1155, Ownable {
-    constructor() ERC1155("") {
-        
-    }
+    constructor() ERC1155("") {}
 
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
